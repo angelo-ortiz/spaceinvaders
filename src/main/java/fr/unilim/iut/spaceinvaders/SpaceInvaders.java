84ef1e0ccfd061ed576364eee72834e31a6118d2
Angelo Ortiz Tandazo <angelo.ortiz@etu.unilim.fr>
2017-05-02 08:52:58 +0200

    Ajout de la fonctionnalité "positionner un vaisseau avec une dimension"

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -3,7 +3,7 @@ package fr.unilim.iut.spaceinvaders;
 import fr.unilim.iut.spaceinvaders.utils.*;
 
 public class SpaceInvaders {
-    
+
     private static final char MARQUE_FIN_LIGNE = '\n';
     private static final char MARQUE_VIDE = '.';
     private static final char MARQUE_VAISSEAU = 'V';
@@ -12,16 +12,23 @@ public class SpaceInvaders {
     Vaisseau vaisseau;
 
     public SpaceInvaders(int longueur, int hauteur) {
-       this.longueur = longueur;
-       this.hauteur = hauteur;
+        this.longueur = longueur;
+        this.hauteur = hauteur;
     }
-    
-    public void positionnerUnNouveauVaisseau(int x, int y) {
-        if (  !estDansEspaceJeu(x, y) ) {
+
+    public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
+        if (!estDansEspaceJeu(x, y)) {
             throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
         }
-            
-        vaisseau = new Vaisseau(x, y);
+        if ( !estDansEspaceJeu(x+longueur-1,y)) {
+            throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers la droite à cause de sa longueur");
+        }
+        if (!estDansEspaceJeu(x,y-hauteur+1)) {
+            throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers le bas à cause de sa hauteur");
+        }
+
+        vaisseau = new Vaisseau(longueur, hauteur);
+        vaisseau.positionner(x, y);
     }
 
     public String recupererEspaceJeuDansChaineASCII() {
@@ -36,25 +43,25 @@ public class SpaceInvaders {
     }
 
     public void deplacerVaisseauVersLaDroite() {
-        if (vaisseau.abscisse() < longueur - 1) {
+        if (vaisseau.abscisseLaPlusADroite() < longueur - 1) {
             vaisseau.seDeplacerVerLaDroite();
         }
     }
 
     public void deplacerVaisseauVersLaGauche() {
-        if (vaisseau.abscisse() > 0) {
+        if (vaisseau.abscisseLaPlusAGauche() > 0) {
             vaisseau.seDeplacerVerLaGauche();
         }
     }
-    
+
     private boolean estDansEspaceJeu(int x, int y) {
         return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
     }
 
     private boolean aUnVaisseau() {
-        return vaisseau!=null;
+        return vaisseau != null;
     }
-    
+
     private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
         return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
     }
@@ -62,9 +69,9 @@ public class SpaceInvaders {
     private char recupererMarqueDeLaPosition(int x, int y) {
         char marque;
         if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
-            marque=MARQUE_VAISSEAU;
+            marque = MARQUE_VAISSEAU;
         else
-              marque=MARQUE_VIDE;
+            marque = MARQUE_VIDE;
         return marque;
     }
 
--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -2,20 +2,48 @@ package fr.unilim.iut.spaceinvaders;
 
 public class Vaisseau {
 
+    int longueur;
+    int hauteur;
     int x;
     int y;
 
-    public Vaisseau(int x, int y) {
+    public Vaisseau(int longueur, int hauteur) {
+        this(longueur, hauteur, 0, 0);
+    }
+    
+    public Vaisseau(int longueur, int hauteur, int x, int y) {
+        this.longueur = longueur;
+        this.hauteur = hauteur;
         this.x = x;
         this.y = y;
     }
 
-    public int abscisse() {
+    public int abscisseLaPlusAGauche() {
         return this.x;
     }
     
+    public int abscisseLaPlusADroite() {
+        return this.x + this.longueur - 1;
+    }
+    
+    private int ordonneeLaPlusHaute() {
+        return this.y;
+    }
+
+    private int ordonneeLaPlusBasse() {
+        return ordonneeLaPlusHaute()-this.hauteur+1;
+    }
+    
     public boolean occupeLaPosition(int x, int y) {
-        return (this.x==x) && (this.y==y);
+        return estAbscisseCouverte(x) && estordonneeCouverte(y);
+    }
+
+    private boolean estordonneeCouverte(int y) {
+        return (ordonneeLaPlusBasse()<=y) && (y<=ordonneeLaPlusHaute());
+    }
+    
+    private boolean estAbscisseCouverte(int x) {
+        return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
     }
 
     public void seDeplacerVerLaDroite() {
@@ -24,6 +52,11 @@ public class Vaisseau {
 
     public void seDeplacerVerLaGauche() {
         this.x --;
+    }
+
+    public void positionner(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
     
 }
